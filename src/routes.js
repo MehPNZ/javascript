68acb295e009edbbeda0fab2e8c5ea2d1ccd0d5d
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-08-18 12:54:11 +0300

    test

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-const host = '/api/v1';
-
-export default {
-  messagesUrl: channelId => [host, 'channels', channelId, 'messages'].join('/'),
-  channelsUrl: () => [host, 'channels'].join('/'),
-  channelUrl: id => [host, 'channels', id].join('/'),
-};
